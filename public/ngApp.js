e26c36cb910b129991d9f6f41388cf00dc028a5b
Samantha Guergenenov <samguergen@gmail.com>
2016-10-03 22:16:41 -0400

    btns as form

--- public/ngApp.js
+++ public/ngApp.js
@@ -1,16 +1,27 @@
-angular
-    .module('myApp', [])
-    .controller('MainCtrl', ['$scope', '$location', '$window', function($scope, $location, $window){
+angular.module('myApp', [])
+    .controller('MainCtrl', ['$scope', '$location', '$window', '$http', function($scope, $location, $window, $http){
+      console.log('inside mainctrl');
 
       $scope.playGame = function(userChoice){
-
+        console.log('inside playgame func');
         var postParam = { 'choice': userChoice };
         var promise = $http.post('/match', postParam);
 
         promise.then( function(response) {
-          $scope.data = response.data;
+          $scope.data = response;
+
+          console.log('response is ', response);
+          console.log('playerChoice is ', response.playerChoice);
+
+
+
+          $scope.results = true;
         });
 
-        if userChoice == choice.rock
       }
+
     }]);
+
+    // playerChoice   : [your choice]
+    // computerChoice : [rock|paper|scissors]
+    // result         : [win|lose|tie]
