676ca94ddf1b1ee6f8e544a28d20916a9677fd06
Samantha Guergenenov <samguergen@gmail.com>
2016-10-03 23:45:52 -0400

    displaying server results on dom

--- public/ngApp.js
+++ public/ngApp.js
@@ -1,28 +1,18 @@
 angular.module('myApp', [])
     .controller('MainCtrl', ['$scope', '$location', '$window', '$http', function($scope, $location, $window, $http){
       console.log('inside mainctrl');
-      $scope.userChoice = {choice: '-'};
+      $scope.userChoice = {choice: ''};
 
       $scope.playGame = function(userSelection){
-        console.log('inside playgame func');
         console.log('userSelection is ', userSelection, $scope.userChoice.choice);
         var postParam = { choice: "" };
         postParam.choice = $scope.userChoice.choice;
         console.log("post param is ", postParam);
-        var config = {
-              headers : {
-                  'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-              }
-          };
         var promise = $http.post('/match', postParam);
 
         promise.then( function(response) {
-          $scope.data = response;
-
-          console.log('response is ', response);
-          console.log('playerChoice is ', response.playerChoice);
-
-
+          $scope.data = response.data;
+          console.log('playerChoice is ', response.data.playerChoice);
 
           $scope.results = true;
         });
