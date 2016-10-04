f35db1a0ecc71f08d61ba37fec8082f70dd66aa2
Samantha Guergenenov <samguergen@gmail.com>
2016-10-04 13:49:53 -0400

    finish comms

--- public/ngApp.js
+++ public/ngApp.js
@@ -1,16 +1,23 @@
 angular.module('myApp', [])
     .controller('MainCtrl', ['$scope','$http', function($scope, $http){
+      //init user choice and score vars
       $scope.userChoice = {choice: ''};
+      $scope.userScore = 0;
 
+      //API call is triggered on click
       $scope.playGame = function(){
         var postParam = { choice: "" };
-        postParam.choice = $scope.userChoice.choice;
-        var promise = $http.post('/match', postParam);
+        postParam.choice = $scope.userChoice.choice; // user choice is updated
+        var promise = $http.post('/match', postParam); //post call send to backend
 
         promise.then( function(response) {
           $scope.data = response.data;
-          $scope.results = true;
-        }, function(error) {
+          //increment/decrement user score and displays on DOM.
+          if ($scope.data.result === 'win'){ $scope.userScore += 1 }
+          else if ($scope.data.result === 'lose'){ $scope.userScore += 1 }
+          $scope.results = true; //display div on DOM with all results
+
+        }, function(error) { // catches error in case result can't be retrieved
           alert("A response could not be retrieved from the server. Try again.", error);
         })
       }
