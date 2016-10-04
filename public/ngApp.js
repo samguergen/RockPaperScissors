ae29f5115c373c519ad00e5ef27f9fa9ad4ce85c
Samantha Guergenenov <samguergen@gmail.com>
2016-10-04 13:37:14 -0400

    promise error handle

--- public/ngApp.js
+++ public/ngApp.js
@@ -1,26 +1,18 @@
 angular.module('myApp', [])
-    .controller('MainCtrl', ['$scope', '$location', '$window', '$http', function($scope, $location, $window, $http){
-      console.log('inside mainctrl');
+    .controller('MainCtrl', ['$scope','$http', function($scope, $http){
       $scope.userChoice = {choice: ''};
 
-      $scope.playGame = function(userSelection){
-        console.log('userSelection is ', userSelection, $scope.userChoice.choice);
+      $scope.playGame = function(){
         var postParam = { choice: "" };
         postParam.choice = $scope.userChoice.choice;
-        console.log("post param is ", postParam);
         var promise = $http.post('/match', postParam);
 
         promise.then( function(response) {
           $scope.data = response.data;
-          console.log('playerChoice is ', response.data.playerChoice);
-
           $scope.results = true;
-        });
-
+        }, function(error) {
+          alert("A response could not be retrieved from the server. Try again.", error);
+        })
       }
 
     }]);
-
-    // playerChoice   : [your choice]
-    // computerChoice : [rock|paper|scissors]
-    // result         : [win|lose|tie]
